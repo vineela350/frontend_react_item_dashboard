6c12aa8917547c00dc854ca8fd4f5e2cc38cfaa6
Vineela Gudipati <vineelagudipati@Vineelas-Air.lan>
2024-02-13 03:36:16 -0500

    this.base_url

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -1,12 +1,12 @@
 class Config {
-    static BASE_URL="https://dashboarditemapi-605432afd181.herokuapp.com/"
-    static loginUrl = BASE_URL+"login/";
-    static createUserUrl = BASE_URL+"register/";
-    static forgotPasswordUrl = BASE_URL+"password_reset/";
-    static fetchItemsForSearchUrl = BASE_URL+"api/items/?search=";
-    static getItemsByCategoryUrl = BASE_URL+"api/items/?category=$";
-    static fetchCategoriesUrl = BASE_URL+"api/categories"
-    static fetchTagsUrl = BASE_URL+"api/tags/";
-    static fetchItemsUrl = BASE_URL+"api/items/";
+    BASE_URL="https://dashboarditemapi-605432afd181.herokuapp.com/"
+    static loginUrl = this.BASE_URL+"login/";
+    static createUserUrl = this.BASE_URL+"register/";
+    static forgotPasswordUrl = this.BASE_URL+"password_reset/";
+    static fetchItemsForSearchUrl = this.BASE_URL+"api/items/?search=";
+    static getItemsByCategoryUrl = this.BASE_URL+"api/items/?category=$";
+    static fetchCategoriesUrl = this.BASE_URL+"api/categories"
+    static fetchTagsUrl = this.BASE_URL+"api/tags/";
+    static fetchItemsUrl = this.BASE_URL+"api/items/";
 
 }
